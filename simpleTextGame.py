15f2158d218d6e2dedd2452854476fc418f20b06
Wutang445 <austinw556@gmail.com>
2020-02-06 11:11:58 -0500

    started simple text game

---
+++ simpleTextGame.py
@@ -0,0 +1,5 @@
+print("********************************************");
+print("***** Welcome to the Rockport Limited! *****");
+print("***Are you ready to begin your adventure?***")
+print("********************************************");
+firstResponse = input();
