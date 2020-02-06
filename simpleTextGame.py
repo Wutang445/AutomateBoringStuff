d79e31f6527981ed01f81646336431f7f6b12051
Wutang445 <austinw556@gmail.com>
2020-02-06 11:18:38 -0500

    working on intro logic

--- simpleTextGame.py
+++ simpleTextGame.py
@@ -1,5 +1,19 @@
+# Function Logic
+
+def introFunction(arg):
+    while(arg != 'y'):
+        if arg == 'stop':
+            print('No, you stop. Party pooper.')
+        print('Are you sure? It\'ll be a good one!')
+        arg = input()
+    
+
+
+# Game Text
+
 print("********************************************");
 print("***** Welcome to the Rockport Limited! *****");
 print("***Are you ready to begin your adventure?***")
 print("********************************************");
-firstResponse = input();
+
+introFunction(input())
