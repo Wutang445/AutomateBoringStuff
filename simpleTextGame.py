dbb01165690aad4d916f9686c6f7cb563804f443
Wutang445 <austinw556@gmail.com>
2020-02-06 11:39:32 -0500

    branching to new repo

--- simpleTextGame.py
+++ simpleTextGame.py
@@ -1,11 +1,19 @@
 # Function Logic
 
 def introFunction(arg):
-    while(arg != 'y'):
+    while(arg != 'yes'):
         if arg == 'stop':
             print('No, you stop. Party pooper.')
-        print('Are you sure? It\'ll be a good one!')
+        elif arg == 'dab':
+            print('*dabs back*')
+        else:
+            print('Are you sure? It\'ll be a good one!')
+            
         arg = input()
+    print('Good! Let us begin then..... \n');
+
+def wagonInteraction(arg):
+
     
 
 
@@ -17,3 +25,6 @@ print("***Are you ready to begin your adventure?***")
 print("********************************************");
 
 introFunction(input())
+
+print("Our story begins as you ride into town, on a wagon with several other rough-looking individuals...")
+print("You don't know any of their names, backgrounds, or where they came from. What do you do? \n")
